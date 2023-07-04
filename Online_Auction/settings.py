51246c60565204f25916ffae8cb2bc76870abdd5
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2023-07-04 14:08:42 +0300

    deploy

--- Online_Auction/settings.py
+++ Online_Auction/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i2fu&hlh(w3j7!9+e9=5dr+_^5ucf+7zsl1fz0nweztyfr&g55'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
